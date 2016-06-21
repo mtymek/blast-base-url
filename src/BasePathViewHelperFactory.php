ae42f61b528193b5ef6a4140f1a0c8e9b79183d8
Matthias Tylkowski <matthias@binarypark.org>
2016-06-21 10:13:48 +0200

    only get parent service locator when inside HelperPluginManager (ZF2) or using it directly (ZF3)

--- src/BasePathViewHelperFactory.php
+++ src/BasePathViewHelperFactory.php
@@ -3,11 +3,17 @@
 namespace Blast\BaseUrl;
 
 use Interop\Container\ContainerInterface;
+use Zend\View\HelperPluginManager;
 
 class BasePathViewHelperFactory
 {
     public function __invoke(ContainerInterface $services)
     {
+        if($services instanceof HelperPluginManager)
+        {
+            $services = $services->getServiceLocator();
+        }
+
         return new BasePathViewHelper(
             $services->get(BasePathHelper::class)
         );
